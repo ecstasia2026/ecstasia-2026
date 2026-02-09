22db4b9cef3da7197e0f3e239322a931cba63230
arpanxneuro <arpanghosh2106@gmail.com>
2026-02-09 17:23:43 +0530

    add Ecstasia26 title in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,31 @@
-
+import { useEffect, useState } from "react";
 
 function Hero() {
+  const [imgScale, setImgScale] = useState(100);
+  const [imgOpacitu, setImgOpacitu] = useState(5);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImgScale(121);
+      setImgOpacitu(100);
+    });
+  }, []);
+
   return (
     <div
-      className="w-full bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/Slice 1.png')" ,
+      className="w-full bg-cover bg-center bg-no-repeat flex items-center justify-center"
+      style={{
+        backgroundImage: "url('/Slice 1.png')",
         aspectRatio: "1238 / 836",
       }}
     >
-
+      <img
+        src="/typo/Ecstasia26_hero_typo.svg"
+        alt="Ecstasia26"
+        className={`w-243 h-104.25 scale-${imgScale} opacity-${imgOpacitu} transition-transform duration-2700 linear`}
+      />
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
