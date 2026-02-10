4d9aa19444ac8b82da2eb044d1758cd47d5dd671
arpanxneuro <arpanghosh2106@gmail.com>
2026-02-10 20:38:49 +0530

    add music toggle; nav responsiveness;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 const twConfig = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
